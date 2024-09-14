c3a25f3703a677794c330b9c3a7100a594335e0a
marchewjapko <marchewjapko@gmail.com>
2024-09-14 04:23:37 +0200

    Boogaloo: part 3

---
+++ WebAPI/RentalManager.Infrastructure/Extensions_2/UserExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace RentalManager.Infrastructure.Extensions;
+
+public static class UserExtensions
+{
+    public static int GetId(this ClaimsPrincipal principal)
+    {
+        return Convert.ToInt32(principal.Claims.FirstOrDefault(c => c.Type == "id")?.Value);
+    }
+}
