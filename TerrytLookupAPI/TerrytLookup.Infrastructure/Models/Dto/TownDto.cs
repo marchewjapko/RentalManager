69c1f2721f542d5c50e6d2da0266203722fe3c1f
marchewjapko <marchewjapko@gmail.com>
2024-11-17 22:09:27 +0100

    Removed TerrytLookupAPI
    TerrytLookupAPI has been migrated to another repository

--- TerrytLookupAPI/TerrytLookup.Infrastructure/Models/Dto/TownDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TerrytLookup.Infrastructure.Models.Dto;
-
-public class TownDto
-{
-    public int Id { get; init; }
-
-    public required string Name { get; init; }
-}
--- TerrytLookupAPI/TerrytLookup.Infrastructure/Models/Dto/VoivodeshipDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TerrytLookup.Infrastructure.Models.Dto;
-
-public class VoivodeshipDto
-{
-    public int Id { get; init; }
-
-    public required string Name { get; init; }
-}
